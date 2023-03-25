3e59bbfdee225b6d29baa3c6cb0188bbaae9b1b8
umbraexy <101243382+umbraexy@users.noreply.github.com>
2023-03-24 22:05:13 -0400

    Update crtsh_domainlist_builder.sh

--- crtsh_domainlist_builder.sh
+++ crtsh_domainlist_builder.sh
@@ -3,7 +3,7 @@
 #Created by umbraexy
 
 
-read -p "Enter the domain to build a wordlist from:" domainaddr;
+read -p "Enter the domain to build a wordlist from: (use x.com, y.org, etc. format for optimal results)" domainaddr;
 curl "https://crt.sh/?q=$domainaddr" | grep -i $domainaddr | sort -u > $domainaddr.wordlist 2> /dev/null;
 sed -i 's/<TD>\|<\/TD>$//g' $domainaddr.wordlist 2> /dev/null;
 sed -i 's/ *//g' $domainaddr.wordlist 2>/dev/null;
